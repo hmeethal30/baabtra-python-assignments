5455f98f06b596bd884a520253276cbe0b5bae13
Haseena <hmeethal30@gmail.com>
2021-03-12 07:29:17 +0530

    code fixes

--- myhtml/views.py
+++ myhtml/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def MyHtml(request):
     return render(request, 'myhtml.html')
+
+def HtmlAssignment1(request):
+# return render(request, 'assignment1.html')
+return render(request, 'assignment1.html')
